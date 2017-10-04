a76641f26adb4ec099d7e4cd98846803b9e057a4
coder98164011 <fanvins@126.com>
2017-10-04 17:38:38 +0800

    第一次提交

---
+++ DBTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(""))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "";
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex);
+                Console.Read();
+            }
+        }
+    }
+}
